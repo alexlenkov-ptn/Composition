88ffa9ed603a021825e70b21c7aa79840124994d
avlnkov <alex.lenkov@mail.ru>
2025-01-14 21:58:26 +0300

    l8: realized game fragment

--- app/src/main/java/presentation/gameFragment/GameFragment.kt
+++ app/src/main/java/presentation/gameFragment/GameFragment.kt
@@ -1,5 +1,6 @@
 package presentation.gameFragment
 
+import android.content.res.ColorStateList
 import android.os.Build
 import android.os.Build.VERSION
 import android.os.Bundle
@@ -7,15 +8,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.composition.R
 import com.example.composition.databinding.FragmentGameBinding
 import domain.entity.GameResult
 import domain.entity.GameSettings
 import domain.entity.Level
 import presentation.GameFinishedFragment
+import ru.sumin.composition.presentation.GameViewModel
 
 class GameFragment : Fragment() {
+
+    private val viewModel: GameViewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+        )[GameViewModel::class.java]
+    }
 
     private var _binding: FragmentGameBinding? = null
     private lateinit var level: Level
@@ -30,6 +42,19 @@ class GameFragment : Fragment() {
             10, 5, 6, 7
         )
     )
+
+    private val tvOptions by lazy {
+        mutableListOf<TextView>().apply {
+            with(binding) {
+                add(tvOption1)
+                add(tvOption2)
+                add(tvOption3)
+                add(tvOption4)
+                add(tvOption5)
+                add(tvOption6)
+            }
+        }
+    }
 
     companion object {
         const val FRAGMENT_NAME = "game_fragment"
@@ -61,9 +86,65 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvSum.setOnClickListener {
-            launchGameResult(gameResultTest)
+        observeViewModel()
+        setClickListenersToOptions()
+        viewModel.startGame(level)
+    }
+
+    private fun setClickListenersToOptions() {
+        tvOptions.forEach { tvOption ->
+            tvOption.setOnClickListener {
+                viewModel.chooseAnswer(tvOption.text.toString().toInt())
+            }
+
         }
+    }
+
+    private fun observeViewModel() {
+        viewModel.question.observe(viewLifecycleOwner) { state ->
+            with(binding) {
+                tvSum.text = state.sum.toString()
+                tvLeftNumber.text = state.visibleNumber.toString()
+
+                for (i in 0 until tvOptions.size) {
+                    tvOptions[i].text = state.options[i].toString()
+                }
+            }
+        }
+
+        viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) { state ->
+            binding.progressBar.setProgress(state, true)
+        }
+
+        viewModel.enoughCount.observe(viewLifecycleOwner) {
+            binding.tvAnswersProgress.setTextColor(getColorByState(it))
+        }
+
+        viewModel.enoughPercent.observe(viewLifecycleOwner) { state ->
+            val color = getColorByState(state)
+            binding.progressBar.progressTintList = ColorStateList.valueOf(color)
+        }
+
+        viewModel.formattedTime.observe(viewLifecycleOwner) { state ->
+            binding.tvTimer.text = state
+        }
+
+        viewModel.minPercent.observe(viewLifecycleOwner) { state ->
+            binding.progressBar.secondaryProgress = state
+        }
+
+        viewModel.gameResult.observe(viewLifecycleOwner) { state ->
+            launchGameResult(state)
+        }
+    }
+
+    private fun getColorByState(state: Boolean): Int {
+        val colorId = if (state) {
+            android.R.color.holo_green_light
+        } else {
+            android.R.color.holo_red_light
+        }
+        return ContextCompat.getColor(requireContext(), colorId)
     }
 
     override fun onDestroy() {
